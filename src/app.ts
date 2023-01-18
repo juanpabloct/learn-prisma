a1e10b1bb6c10f91fdc6e9e9309883f30319e8c8
juanpabloct <juanptorres2003@gmail.com>
2023-01-17 22:39:09 -0500

    borro importaacion innecesaria

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from "express";
-import { ParamsBody } from "../interface/paramsbody";
 import PrismaConnection from "./conection/client";
 
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.get("/", async (req: Request, res: Response) => {
-  const data = req.body as ParamsBody;
-  const prisma = await PrismaConnection.usuario.create({ data });
+  const prisma = await PrismaConnection.usuario.create({ data: req.body });
 
   res.json("");
 });
