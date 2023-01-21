feea149cd8db084fd6140da306559d5727874556
juanpabloct <juanptorres2003@gmail.com>
2023-01-21 00:40:23 -0500

    Crear partes pequeñas de rutas

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,7 @@
-import { Request, Response } from "express";
-import PrismaConnection from "./conection/client";
-
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.get("/", async (req: Request, res: Response) => {
-  const prisma = await PrismaConnection.usuario.create({ data: req.body });
-
-  res.json("");
-});
+app.use("/user", require("../routes/user/index"));
 app.listen(9000, () => {
   console.log("🚀Corriendo en el puerto 9000🚀");
 });
